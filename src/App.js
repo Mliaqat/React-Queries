a1e1e9d0bd40ed00919c0d6652c3d0f671dab8b5
Liaqat Shehzad <liaqat.shehzad@ceative.co.uk>
2023-01-16 17:28:51 +0500

    UseQuery

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
+import { QueryClient, QueryClientProvider } from "react-query";
 import { Route, Routes } from "react-router-dom";
 import Home from "./Component/Home/Home";
 import SimpleFetch from "./Component/SimpleFetch/SimpleFetch";
 import User from "./Component/User/User";
 
+const queryClint= new QueryClient()
+
 function App() {
+
   return (
+    <QueryClientProvider client={queryClint}>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="react-query" element={<User />} />
       <Route path="simple-fetch" element={<SimpleFetch />} />
     </Routes>
+    </QueryClientProvider>
   );
 }
 
--- src/Component/User/User.js
+++ src/Component/User/User.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import axios from "axios";
+import React from "react";
+import { useQuery } from "react-query";
+
+const fectchUser = () => {
+  return axios.get("http://localhost:4000/users");
+};
 
 function User() {
+  const { isLoading, data } = useQuery("user", fectchUser);
+
+  if (isLoading) {
+    return <h2>Loading....</h2>;
+  }
   return (
-    <div>User</div>
-  )
+    <div>
+      {data?.data.map((data) => {
+        return <h3 key={data.id}>{data.name}</h3>;
+      })}
+    </div>
+  );
 }
 
 export default User;
